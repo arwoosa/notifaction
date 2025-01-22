f99279fe9020a962418b270eec49134b70cc5f60
Chen Yan Jui <ch.focke@gmail.com>
2025-01-22 11:15:06 +0800

    add create notification validate

--- router/request/createNotification.go
+++ router/request/createNotification.go
@@ -1,5 +1,7 @@
 package request
 
+import "errors"
+
 type CreateNotification struct {
 	To    []string          `json:"to"`
 	From  string            `json:"from"`
@@ -8,6 +10,17 @@ type CreateNotification struct {
 }
 
 func (r *CreateNotification) Validate() error {
-	// TODO: complete
+	if len(r.To) == 0 {
+		return errors.New("empty to")
+	}
+	if r.From == "" {
+		return errors.New("empty from")
+	}
+	if r.Event == "" {
+		return errors.New("empty event")
+	}
+	if r.Data == nil {
+		return errors.New("empty data")
+	}
 	return nil
 }
